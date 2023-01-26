68a17fea6011ff3e2f1701f25c60b0df4853158a
MaisarahQistina <93722776+MaisarahQistina@users.noreply.github.com>
2023-01-26 16:13:54 +0800

    Add start button
    Start button added and linked, but game cannot move

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,54 @@
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyListener;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 public class Main {
 	public static void main(String[] args) {
 		JFrame obj=new JFrame();
-		Gameplay gamePlay = new Gameplay();
+	//	Gameplay gamePlay = new Gameplay();
 		
 		obj.setBounds(10, 10, 700, 600);
-		obj.setTitle("Breakout Ball");		
+		obj.setTitle("BRICK BREAKER");		
 		obj.setResizable(false);
 		obj.setVisible(true);
 		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		obj.add(gamePlay);
-                obj.setVisible(true);
 		
+		// Create a JMenuBar
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setVisible(true);
+
+		JMenu menu = new JMenu("Brick Breaker");
+		
+	    // Create a JMenuItem
+	    JMenuItem menuItem1 = new JMenuItem("START");
+		
+      // Set the menu bar for the frame
+      menu.add(menuItem1);
+    
+      
+      menuBar.add(menu);
+      
+      obj.setJMenuBar(menuBar);
+      
+      // Set the size and visibility of the frame
+     obj.setSize(1000, 780);
+      obj.setVisible(true);
+
+      
+      menuItem1.addActionListener(new ActionListener() {
+    	  public void actionPerformed(ActionEvent e) {
+    		Gameplay gamePlay = new Gameplay();
+      		obj.add(gamePlay);
+      		obj.setVisible(true);
+      }
+      });
+      
 	}
 
 }
