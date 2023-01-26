3f897f7753d9c1942aa26091c66bdcf83f6288e9
anisfrhh <117010328+anisfrhh@users.noreply.github.com>
2023-01-26 16:12:09 +0800

    Update MapGenerator.java

--- src/MapGenerator.java
+++ src/MapGenerator.java
@@ -7,16 +7,70 @@ public class MapGenerator
 	public int map[][];
 	public int brickWidth;
 	public int brickHeight;
+	public int maprow,mapcol;
 	
-	public MapGenerator (int row, int col)
+	public int numGreen=0;
+	public int numRed=0;
+	
+	public MapGenerator (int row,int col)
 	{
-		map = new int[row][col];		
+		maprow=row;
+		mapcol=col;
+		map = new int[maprow][mapcol];		
 		for(int i = 0; i<map.length; i++)
 		{
 			for(int j =0; j<map[0].length; j++)
 			{
-				map[i][j] = 1;
+				//generate random number to assign diff colour
+				int r=(int)(Math.random()*5+1);
+				map[i][j] = r;
+				if (map[i][j] == 4) {
+					numGreen+=1;
+				} else if (map[i][j] == 5) {
+						numRed+=1;
+					   }	
+			}		
+		}
+		
+		
+		if(numGreen> 4) {
+		A:for(int i = 0; i<map.length; i++)
+		{
+			for(int j =0; j<map[0].length; j++)
+			{
+				if (map[i][j]== 4) {
+					//generate random number to assign diff colour
+					int r=(int)(Math.random()*3+1);
+					map[i][j] = r;
+					numGreen-=1;
+					
+					if(numGreen==3)
+					{ break A;}
+				}
+				
 			}			
+		}
+		
+		}
+		
+		if(numRed> 4) {
+		A:for(int i = 0; i<map.length; i++)
+		{
+			for(int j =0; j<map[0].length; j++)
+			{
+				if (map[i][j]== 5) {
+					//generate random number to assign diff colour
+					int r=(int)(Math.random()*3+1);
+					map[i][j] = r;
+					numRed-=1;
+					
+					if(numRed==3)
+					{ break A;}
+				}
+				
+			}			
+		}
+		
 		}
 		
 		brickWidth = 540/col;
@@ -31,7 +85,25 @@ public class MapGenerator
 			{
 				if(map[i][j] > 0)
 				{
-					g.setColor(Color.white);
+					//g.setColor(Color.white);
+					
+					if (map[i][j]==1) {
+						g.setColor(new Color(204,102,0));
+					}
+					if (map[i][j]==2) {
+						g.setColor(new Color(255,128,0));
+					}
+					if (map[i][j]==3) {
+						g.setColor(new Color(255,153,51));
+					}
+					//WIDEPADDLE
+					if (map[i][j]==PowerUp.WIDEPADDLE) {
+						g.setColor(PowerUp.WIDECOLOR);
+					}
+					//FASTBALL
+					if (map[i][j]==PowerUp.FASTBALL) {
+						g.setColor(PowerUp.FASTCOLOR);
+					}
 					g.fillRect(j * brickWidth + 80, i * brickHeight + 50, brickWidth, brickHeight);
 					
 					// this is just to show separate brick, game can still run without it
@@ -43,8 +115,36 @@ public class MapGenerator
 		}
 	}
 	
+	//func to detect win situation
+	public boolean isThereWin() {
+		boolean thereIsWin=false;
+		int bricksRemaining=0;
+		for (int row=0;row<map.length;row++) {
+			for(int col=0;col<map[0].length;col++) {
+				
+				bricksRemaining+=map[row][col];
+			}
+		}
+		
+		if(bricksRemaining==0) {
+			thereIsWin=true;
+		}
+		
+		return thereIsWin;
+	}
+	
+	//utk tukar warna,func ni tk guna sbb kita ganti dgn hitbrick func--//guna doh dlm powerup
 	public void setBrickValue(int value, int row, int col)
 	{
 		map[row][col] = value;
 	}
+	
+	//set the reduction of the colour
+	public void hitBrick(int row,int col) {
+		map[row][col]-=1;
+		if (map[row][col]<0) {
+			map[row][col]=0;
+		}
+	}
+	
 }
